8088e5b3555ad6052688f5e2d2252a7964e0158e
Prashant Shrestha <119914099+din0115@users.noreply.github.com>
2023-06-13 15:29:59 +0545

    script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+const searchBox = document.querySelector(".search-box");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const searchInput = document.querySelector("input");
+const searchData = document.querySelector(".search-data");
+searchBtn.onclick =()=>{
+  searchBox.classList.add("active");
+  searchBtn.classList.add("active");
+  searchInput.classList.add("active");
+  cancelBtn.classList.add("active");
+  searchInput.focus();
+  if(searchInput.value != ""){
+    var values = searchInput.value;
+    searchData.classList.remove("active");
+    searchData.innerHTML = "You just typed " + "<span style='font-weight: 500;'>" + values + "</span>";
+  }else{
+    searchData.textContent = "";
+  }
+}
+cancelBtn.onclick =()=>{
+  searchBox.classList.remove("active");
+  searchBtn.classList.remove("active");
+  searchInput.classList.remove("active");
+  cancelBtn.classList.remove("active");
+  searchData.classList.toggle("active");
+  searchInput.value = "";
+}
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
